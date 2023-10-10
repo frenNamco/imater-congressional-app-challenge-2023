f094a699177eb3fe4eed262bdf7d59f79dd4ef52
Fernando Aragon <121034258+frenNamco@users.noreply.github.com>
2023-10-10 23:14:45 +0000

    Some more work

--- my-app/src/js/loadingScreen.js
+++ my-app/src/js/loadingScreen.js
@@ -1,4 +1,5 @@
-const startButton = document.querySelector(".start-button");
+//This should be working but I can't tell so please fix it
+const startButton = document.getElementbyId("start-button");
 const loadingScreen = document.getElementById("loading-screen");
 
 startButton.addEventListener("click", () => {
