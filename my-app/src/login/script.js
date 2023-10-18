c7d4951fb530387bafe04a2f5d09c2ec656c7a69
Fernando Aragon <121034258+frenNamco@users.noreply.github.com>
2023-10-17 21:16:08 -0400

    Finished the login screen (for the most part)

--- my-app/src/login/script.js
+++ my-app/src/login/script.js
@@ -1,34 +1,52 @@
-textboxes = document.getElementsByClassName("textbox");
-button = document.getElementById("login-button");
+const textboxes = document.getElementsByClassName("textbox");
+const button = document.getElementById("login-button");
 
-emails = ["fake4625@gmail.com", "jlope@imater.org", "test@test.com"];
-passwords = ["littlebabyman", "ballsman", "test"];
+const emailTextbox = textboxes[0];
+const passwordTextbox = textboxes[1];
+
+const emails = ["fake4625@gmail.com", "jlope@imater.org", "test@test.com"];
+const passwords = ["littlebabyman", "ballsman", "test"];
 
 button.addEventListener("click", () => {
-    let inputEmail = textboxes[0].value;
-    let inputPass = textboxes[1].value;
-    let emailInvalid = false;
-    let passInvalid = false;
-    let emailIndex = 0;
-    let passIndex = 0;
+    let inputEmail = emailTextbox.value;
+    let inputPass = passwordTextbox.value;
+    let emailInvalid = true;
+    let passInvalid = true;
+    let emailIndex = -1;
+    let passIndex = -1;
 
-    emails.forEach((email, index) => {
-        if (email == inputEmail) {
-            emailIndex = index;
+    for (let i = 0; i < emails.length; i++) {
+        const email = emails[i];
+      
+        if (email === inputEmail) {
+            emailIndex = i;
+            emailInvalid = false;
+            break;
         } else {
             emailInvalid = true;
         }
-    });
+    }
 
-    passwords.forEach((password, index) => {
-        if (password == inputPass) {
-            passIndex = index;
+    for (let i = 0; i < passwords.length; i++) {
+        const password = passwords[i];
+      
+        if (password === inputPass) {
+            passIndex = i;
+            passInvalid = false;
+            break;
         } else {
             passInvalid = true;
         }
-    })
+    }
 
     if ((emailIndex == passIndex) && !(emailInvalid || passInvalid)){
         window.location.href = "https://www.merriam-webster.com/dictionary/victory";
+
+    } else {
+        emailTextbox.value = "";
+        emailTextbox.style.border = '1px solid red';
+
+        passwordTextbox.value = "";
+        passwordTextbox.style.border = '1px solid red';
     }
 })
