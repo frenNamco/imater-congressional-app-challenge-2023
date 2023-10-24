0934faea29bf5d923758050471f220f6587eac3b
Fernando Aragon <121034258+frenNamco@users.noreply.github.com>
2023-10-23 20:02:32 -0400

    Delete my-app/src/js/loadingScreen.js

--- my-app/src/js/loadingScreen.js
+++
@@ -1,7 +0,0 @@
-//This should be working but I can't tell so please fix it
-const startButton = document.getElementbyId("start-button");
-const loadingScreen = document.getElementById("loading-screen");
-
-startButton.addEventListener("click", () => {
-  loadingScreen.classList.toggle("active");
-});
