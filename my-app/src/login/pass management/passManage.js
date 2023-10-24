f98e03f24fefb8dc8e87c29c1dcc0df00ec78c59
Fernando Aragon <121034258+frenNamco@users.noreply.github.com>
2023-10-24 16:02:38 -0400

    New page, added functionality to admin page

--- my-app/src/login/pass management/passManage.js
+++ my-app/src/login/pass management/passManage.js
@@ -1,8 +1,28 @@
 const welcomeText = document.getElementById("welcome-text");
 const roomCodeInput = document.getElementById("room-code-input");
 const roomCodeButton = document.getElementById("setRoomCode");
+const classPasses = document.getElementById("class-passes");
+const classPass1 = document.getElementById("pass-1");
+const classPass2 = document.getElementById("pass-2");
+const classPass3 = document.getElementById("pass-3");
+const classPass4 = document.getElementById("pass-4");
+const classPass5 = document.getElementById("pass-5");
+const classPass6 = document.getElementById("pass-6");
+const studentPassContainer = document.getElementsByClassName("student-pass-container");
+const studentText = document.getElementsByClassName(
+    "student-container");
 
 const teacherIndex = parseInt(localStorage.getItem("active"));
+
+let passes = {};
+
+if (localStorage.getItem("passes") === null) {
+    for (let i = 0; i < studentText.length; i++) {
+        studentText[i].textContent = "Create Pass";
+    }
+} else {
+    passes = parseJSON(localStorage.getItem("passes"));
+}
 
 let teacherJSON; 
 fetch('../../data/teacher.json').then(response => response.json()).then(data => {
@@ -15,10 +35,18 @@ fetch('../../data/teacher.json').then(response => response.json()).then(data => 
 })
 
 roomCodeButton.addEventListener("click", () => {
-    const roomCode = roomCodeInput.value;
+    roomCode = roomCodeInput.value;
+    classPasses.style.display = "grid";
     
     localStorage.setItem(roomCode.toString(), teacherIndex.toString());
     alert("Room code has been set: " + roomCode + "\n" + 
     "by: " + teacherJSON.teachers[teacherIndex].name);
 })
 
+classPass1.addEventListener("click", () => {
+    const passLocation = 1;
+
+    localStorage.setItem("location", passLocation.toString());
+    window.location.href = "../../pass create/pass-create.html";
+
+})
